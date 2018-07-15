1ee6411686c2d41f9291ff385e72dd0a35209da1
Atlas-owo <atlas_w@outlook.com>
2018-07-15 11:24:17 +0800

    流程控制

--- S4/src/process_control/switch语句.java
+++ S4/src/process_control/switch语句.java
@@ -4,6 +4,30 @@ public class switch”Ôæ‰ {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a[] = {1,2,3,4,5,6};
+		for(int x : a){
+			switch(x){
+			case 1:
+				System.out.println(1);
+				break;
+			case 2:
+				System.out.println(2);
+				break;
+			case 3:
+				System.out.println(3);
+				break;
+			case 4:
+				System.out.println(4);
+				break;
+			case 5:
+				System.out.println(5);
+				break;
+			default:
+				System.out.println("none");
+				break;
+			
+			}
+		}
 		
 	}
 
